c9d2d4955183f474d94929c0a7f6c23720081d8d
Joey Adams <joeyadams3.14159@gmail.com>
2011-09-11 00:53:13 -0400

    test.hs: Finished implementing prop_match_similar.

--- test.hs
+++ test.hs
@@ -1,13 +1,17 @@
 import BDelta (diff, patch)
 
 import Control.Applicative ((<$>))
+import Control.Monad (replicateM)
 import Data.Function (on)
 import Data.List (sortBy)
+import Data.String (IsString(fromString))
 import Data.Word (Word8)
 import Test.QuickCheck (Arbitrary(arbitrary), CoArbitrary(coarbitrary),
-                        Testable, quickCheckWith, stdArgs, Args(maxSize))
+                        Testable, quickCheckWith, stdArgs, Args(maxSize),
+                        choose)
 
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C
 
 newtype ByteString = Wrap {unwrap :: B.ByteString}
 
@@ -20,10 +24,21 @@ unpack = B.unpack . unwrap
 instance Show ByteString where
     show = show . unwrap
 
+instance IsString ByteString where
+    fromString = Wrap . C.pack
+
 instance Arbitrary ByteString where
     arbitrary = pack <$> arbitrary
 instance CoArbitrary ByteString where
     coarbitrary = coarbitrary . unpack
+
+prop_match :: ByteString -> ByteString -> Bool
+prop_match (Wrap a) (Wrap b) = patch a (diff a b) == Just b
+
+prop_equal :: ByteString -> Bool
+prop_equal (Wrap s) =
+    let d = diff s s
+    in B.length d < 10 && patch s d == Just s
 
 data Edit = Insert Int Word8
           | Delete Int
@@ -41,17 +56,36 @@ applyEditString editString (Wrap string) =
                 let (a, b) = B.splitAt (i - pos) str
                  in a : loop (i+1) (B.tail b) edits
             | otherwise = loop pos str edits
-        editIndex (Insert idx _) = idx
-        editIndex (Delete idx)   = idx
-     in Wrap $ B.concat $ loop 0 string (sortBy (compare `on` editIndex) editString)
+        editKey (Insert idx _) = (idx, 0 :: Int)
+        editKey (Delete idx)   = (idx, 1 :: Int)
+     in Wrap $ B.concat $ loop 0 string (sortBy (compare `on` editKey) editString)
 
-prop_match :: ByteString -> ByteString -> Bool
-prop_match (Wrap a) (Wrap b) = patch a (diff a b) == Just b
+data Similar = Similar ByteString ByteString
+    deriving Show
 
-prop_equal :: ByteString -> Bool
-prop_equal (Wrap s) =
-    let d = diff s s
-    in B.length d < 10 && patch s d == Just s
+instance Arbitrary Similar where
+    arbitrary = do
+        old <- arbitrary
+        let len = B.length $ unwrap old
+        
+        -- Choose length of edit string, favoring small sizes.
+        c <- choose (0, len)
+        c' <- choose (0, c)
+        
+        editString <- replicateM c' $ do
+            -- This is a little tricky.  op = 0 means insert, while op = 1 means delete.
+            -- We can insert on indices 0..len, but we can only delete on 0..len-1.
+            -- If the string is empty, we can't delete.
+            op <- choose (0 :: Int, if len > 0 then 1 else 0)
+            pos <- choose (0, len - op)
+            case op of
+                 0 -> Insert pos <$> arbitrary
+                 _ -> return (Delete pos)
+        
+        return $ Similar old (applyEditString editString old)
+
+prop_match_similar :: Similar -> Bool
+prop_match_similar (Similar (Wrap a) (Wrap b)) = patch a (diff a b) == Just b
 
 main :: IO ()
 main = do
@@ -59,4 +93,5 @@ main = do
         qc = quickCheckWith stdArgs {maxSize = 1000}
     qc prop_match
     qc prop_equal
+    qc prop_match_similar
 
